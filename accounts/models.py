a12d8e349d7d651130207b039913d1a333d7bbff
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-11 12:10:19 +0330

    change fun _create_user

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser,UserManager
 class MyUserManager(UserManager):
     def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         username = extra_fields['national_code']
-        return self._create_user(username, email, password, **extra_fields)
+        return self.create_user(username, email, password, **extra_fields)
 
 class User(AbstractUser):
    national_code = models.CharField( max_length=10,null =True,blank=True,unique=True)
