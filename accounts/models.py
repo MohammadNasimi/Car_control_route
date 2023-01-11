80db1da698113a49cea339c39a31b9962a500d61
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-11 12:08:29 +0330

    create user or owner car models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser,UserManager
+
+class MyUserManager(UserManager):
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields['national_code']
+        return self._create_user(username, email, password, **extra_fields)
+
+class User(AbstractUser):
+   national_code = models.CharField( max_length=10,null =True,blank=True,unique=True)
+   age = models.PositiveIntegerField(null=True,blank=True)
+   total_toll_paid = models.PositiveIntegerField(null=True,blank=True,default=0)
+   USERNAME_FIELD = 'national_code'
+
+   objects =MyUserManager()    
