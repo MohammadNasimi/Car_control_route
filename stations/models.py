93341c8e6f69ddf92e1d40f7e8df81b460744149
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-13 23:41:39 +0330

    create model stations

--- stations/models.py
+++ stations/models.py
@@ -1,3 +1,16 @@
+from django.contrib.gis.db import models as gis_models
 from django.db import models
 
 # Create your models here.
+class Stations(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=15,null=False,blank=False)
+    toll_per_cross = models.PositiveBigIntegerField(null=False,blank=False)
+    location = gis_models.PointField()
+    
+
+
+
+    def __str__(self):
+        return self.name
+
