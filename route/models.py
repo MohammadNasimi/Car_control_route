120aa136a9a5062bac9b3ed82a08c2e98b88ba99
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-13 23:50:34 +0330

    create model Route

--- route/models.py
+++ route/models.py
@@ -1,3 +1,12 @@
+from django.contrib.gis.db import models as gis_models
 from django.db import models
 
 # Create your models here.
+class Route(models.Model):
+    name = models.CharField(max_length=15,null=False,blank=False)
+    width = models.PositiveBigIntegerField(null=False,blank=False)
+    geom = gis_models.MultiLineStringField()
+    
+
+    def __str__(self):
+        return self.name
