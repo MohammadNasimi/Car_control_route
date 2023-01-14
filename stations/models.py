bc24b2666d04b7d315646d4876d85905f9dd1ea3
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-14 23:54:07 +0330

    create toll models

--- stations/models.py
+++ stations/models.py
@@ -1,15 +1,19 @@
 from django.contrib.gis.db import models as gis_models
 from django.db import models
-
+from car.models import Car
 # Create your models here.
 class Stations(models.Model):
     name = models.CharField(max_length=15,null=False,blank=False)
     toll_per_cross = models.PositiveBigIntegerField(null=False,blank=False)
-    location = gis_models.PointField()
+    #location = gis_models.PointField()
     
-
-
-
     def __str__(self):
         return self.name
-
+# toll car 
+class Toll(models.Model):
+    car = models.ForeignKey(Car,on_delete=models.CASCADE,null=False,blank=False)
+    station = models.ForeignKey(Stations,on_delete=models.CASCADE,null=False,blank=False)
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.car
