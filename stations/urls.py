b22406e70c493dd43f38041e188e997f37bf197b
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-15 00:08:55 +0330

    add comments

--- stations/urls.py
+++ stations/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from stations.views import ListTollView
 urlpatterns = [
+    #list toll car or user in Specific times
     path('list/toll/', ListTollView.as_view(), name='list_toll'),
     ]
