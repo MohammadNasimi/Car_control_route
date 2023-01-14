88e27b9612f9585517b97b02c9ddf7ed88566063
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-14 22:38:16 +0330

    CarFineserializer

--- car/serializers.py
+++ car/serializers.py
@@ -1,9 +1,15 @@
 
 from rest_framework import serializers
-from car.models import Car
+from car.models import Car,CarFine
 #ser accounts model 
 class Carserializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ('id', 'owner', 'type','color','length', 'load_valume')
         read_only_fields = ['id','owner']
+        
+class CarFineserializer(serializers.ModelSerializer):
+    car = Carserializer(read_only = True)
+    class Meta:
+        model = CarFine
+        fields = ('id', 'car','route')
