bcd49943a0f9622d105eeef96a5de8c1e24c11f9
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-14 16:09:11 +0330

    list all car color red or blue urls

--- car/urls.py
+++ car/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-from car.views import CreateCarView
+from car.views import CreateCarView,ListCarView
 urlpatterns = [
     path('list/', CreateCarView.as_view(), name='list_car'),
+    path('list/color/',ListCarView.as_view(),name='list_color_car'),#list color red and blue car with out athenticate
 ]
