e875ed93e0a6bfc6c32c1d11f97c5178f3b61880
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-14 22:41:48 +0330

    carfine urls

--- car/urls.py
+++ car/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('list/', CreateCarView.as_view(), name='list_car'),
     path('list/color/',ListCarColorView.as_view(),name='list_color_car'),#list color red and blue car with out athenticate
     path('list/age/',ListCarAgeownerView.as_view()),# list car owner's age greater than 70
+    path('list/carfine/',ListCarAgeownerView.as_view()),# list carfine across route width greater than 20 meters
+
     ]
