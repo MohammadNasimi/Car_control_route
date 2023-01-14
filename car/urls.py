4129aea74a3638afc91f0083fd69aac6a50e451b
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-14 16:26:20 +0330

    age 70 urls

--- car/urls.py
+++ car/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from car.views import CreateCarView,ListCarView
+from car.views import *
 urlpatterns = [
     path('list/', CreateCarView.as_view(), name='list_car'),
-    path('list/color/',ListCarView.as_view(),name='list_color_car'),#list color red and blue car with out athenticate
-]
+    path('list/color/',ListCarColorView.as_view(),name='list_color_car'),#list color red and blue car with out athenticate
+    path('list/age/',ListCarAgeownerView.as_view()),# list car owner's age greater than 70
+    ]
