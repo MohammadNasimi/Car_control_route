e78c298343341b8b0e750bc9f83559625f99b374
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-14 12:57:31 +0330

    remove filter setting rest

--- car_control_config/settings.py
+++ car_control_config/settings.py
@@ -158,6 +158,5 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 
 }
