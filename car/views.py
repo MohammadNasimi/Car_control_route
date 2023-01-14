3334f0e825a69e3c1188173710c432c9baeb8634
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-14 15:46:16 +0330

    remove print

--- car/views.py
+++ car/views.py
@@ -33,7 +33,6 @@ class CreateCarView(ListCreateAPIView):
                 return Response({'user':'you have one big car'}, status=status.HTTP_400_BAD_REQUEST)
             except Car.DoesNotExist:
                 pass
-                print(1)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
