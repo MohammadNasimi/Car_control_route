63cda398d125404726f693e66cc41c6945efa3d2
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-15 11:32:23 +0330

    ListUserTollView urls

--- stations/urls.py
+++ stations/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
-from stations.views import ListTollView
+from stations.views import ListTollView,ListUserTollView
 urlpatterns = [
     #list toll car or user in Specific times
     path('list/toll/', ListTollView.as_view(), name='list_toll'),
+    # list users have toll  greater than 0 and order by it with values total_toll_paid ASC
+    path('list/usertoll/', ListUserTollView.as_view(), name='list_user_toll'),
+    
+
     ]
