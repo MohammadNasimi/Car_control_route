5d6d398c72c2e6bc9caf8a9bc8b07234a1b8d0bc
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-15 15:38:18 +0330

    docs log in

--- accounts/docs.py
+++ accounts/docs.py
@@ -1,5 +1,14 @@
 log_in_post ="""
+you post  national_code and password for log in :
+and for access another urls with add
+access token in bearer token
+model json :
 
+    {
+    "national_code": "1520430353",
+    "password": "1"
+    }
+    
 """
 register_post =""""
 
