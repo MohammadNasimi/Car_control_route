510f3fd423e847d7d06311c956b9c3d795725024
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-15 21:55:56 +0330

    docs stations

--- stations/docs.py
+++ stations/docs.py
@@ -1,6 +1,20 @@
 toll_list_get ="""
+user should log in  super user
+list toll  with id car or user 
+params :
+    search_key :
+                user --> filter toll with user id 
+                car --> filter toll with car id 
+    date_from:
+            after this date show toll
+    date_to: 
+            before this date show toll
+    if you use both of date_from date_to 
+    list toll between dates
 
 """
 toll_user_list_get ="""
+list users have toll asc total_toll_paid
+user's total_toll_paid greater than zero 
 
 """
