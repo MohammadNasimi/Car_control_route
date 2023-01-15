174cad20d96752229c1ab94b382f6ab6ec4663d9
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-15 17:57:24 +0330

    add id in urls

--- stations/urls.py
+++ stations/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from stations.views import ListTollView,ListUserTollView
 urlpatterns = [
     #list toll car or user in Specific times
-    path('list/toll/', ListTollView.as_view(), name='list_toll'),
+    path('list/toll/<int:id>/', ListTollView.as_view(), name='list_toll'),
     # list users have toll  greater than 0 and order by it with values total_toll_paid ASC
     path('list/usertoll/', ListUserTollView.as_view(), name='list_user_toll'),
     
