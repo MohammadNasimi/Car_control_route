d614c3d39557ec9f3b929013a40a1fe780d2e589
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-15 11:55:21 +0330

    swagger set in urls

--- car_control_config/urls.py
+++ car_control_config/urls.py
@@ -16,9 +16,45 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from rest_framework import permissions 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi 
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+class CustomOpenAPISchemaGenerator(OpenAPISchemaGenerator):
+  def get_schema(self, request=None, public=False):
+    """Generate a :class:.Swagger object with custom tags"""
+
+    swagger = super().get_schema(request, public)
+    swagger.tags = [
+        {
+            "name": "Car_control",
+            "description": "car_control  API"
+        },
+    ]
+
+    return swagger
+
+schema_view = get_schema_view( 
+openapi.Info(
+title="Blog API",
+default_version="v1",
+description="A sample API for learning DRF",
+terms_of_service="https://www.google.com/policies/terms/",
+contact=openapi.Contact(email="hello@example.com"),
+license=openapi.License(name="BSD License"),
+),
+public=True,
+permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('car/',include('car.urls')),
     path('stations/',include('stations.urls')),
+    path('swagger/', schema_view.with_ui( 
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui( 
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
