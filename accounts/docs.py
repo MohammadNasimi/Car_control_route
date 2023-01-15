8ac06e678fe4451bac649a056ee4f1c6e8e06f4b
mohammadnasimi <mohammadnasimi6803@gmail.com>
2023-01-15 15:56:54 +0330

    doss registers

--- accounts/docs.py
+++ accounts/docs.py
@@ -1,8 +1,8 @@
 log_in_post ="""
 you post  national_code and password for log in :
-and for access another urls with add
+for access another urls with add
 access token in bearer token
-model json :
+model log in sample :
 
     {
     "national_code": "1520430353",
@@ -12,4 +12,18 @@ model json :
 """
 register_post =""""
 
+All field should fill and password equal with password2
+and national_codes are unique
+model registers sample:
+
+
+    {
+    "first_name": "ali",
+    "national_code": "1521234567",
+    "password": "1521234567",
+    "password2":"1521234567",
+    "age":25
+    }
+    
+    
 """
